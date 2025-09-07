8f9dda6ed62d83064df1a46d3bf2b22ac7b6b210
joegibsonoil12-source <joegibsonoil12@gmail.com>
2025-09-07 00:49:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// IMPORTANT: match your GitHub repo name here
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/KPI-Dashboard/",
   plugins: [react()],
-});
+  base: '/KPI-Dashboard/', // IMPORTANT: match your repo name exactly
+})
