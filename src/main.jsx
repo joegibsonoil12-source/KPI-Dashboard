81fb820388d0360a0094df6cc9f0d4c4625d75ae
joegibsonoil12-source <joegibsonoil12@gmail.com>
2025-09-10 15:34:15 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import AuthLayout from "./AuthLayout.jsx";
+import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <AuthLayout>
-      <App />
-    </AuthLayout>
+    <App />
   </React.StrictMode>
 );
