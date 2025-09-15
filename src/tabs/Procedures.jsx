d2aa961c478ba119db8fdd0fcbbe66fe1fad39ae
joegibsonoil12-source <joegibsonoil12@gmail.com>
2025-09-15 10:54:52 -0400

    Update Procedures.jsx

--- src/tabs/Procedures.jsx
+++ src/tabs/Procedures.jsx
@@ -1,2 +1,7 @@
 // src/tabs/Procedures.jsx
 export { default } from './Procedures_v3.jsx'
+// near the top with other imports
+import DebugDrawer from './components/DebugDrawer'
+
+// ...inside your App component JSX (anywhere; bottom of the page is fine)
+<DebugDrawer />
