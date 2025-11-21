956b4f4719f5744223c653d7af34c0eb3a6f9677
joegibsonoil12-source <joegibsonoil12-source@users.noreply.github.com>
2025-11-21 17:13:14 +0000

    deploy: 41cca1a17bc571f2a0b0a4b7c6051ecf9cb75bde

---
+++ runtime-config.js
@@ -0,0 +1,10 @@
+(function () {
+  window.__ENV = {
+  "BASE_PATH": "/KPI-Dashboard",
+  "API_BASE": "",
+  "BILLBOARD_API_BASE": "",
+  "BILLBOARD_TV_TOKEN": "",
+  "VITE_SUPABASE_URL": "",
+  "VITE_SUPABASE_ANON_KEY": ""
+};
+})();
